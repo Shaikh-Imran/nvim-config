237de3e21636bb519948280140a024f04fd49433
Imran Shaikh <imran@coderstea.in>
2023-05-26 12:32:47 +0530

    GitIngored file shown in nvimtree

--- lua/configs/nvim-tree-config.lua
+++ lua/configs/nvim-tree-config.lua
@@ -1,5 +1,11 @@
-require'nvim-tree'.setup {
-    diagnostics = {
-        enable = true
-    }
-}
+require("nvim-tree").setup({
+	git = {
+		enable = false,
+		ignore = true,
+		timeout = 500,
+	},
+
+	diagnostics = {
+		enable = true,
+	},
+})
